4b4fa7a3586c28fe62341940871e6f26199d25c0
wcheng <wcheng@fedora27.localdomain>
2018-01-22 22:23:48 +0800

    chapter 6 added some function

--- chapter6.lisp
+++ chapter6.lisp
@@ -8,3 +8,34 @@
 (defun our-funcall (fn &rest arguments)
   (apply fn arguments))
 
+(defun single? (lst)
+  (and (consp lst) (null (cdr lst))))
+
+(defun append1 (lst obj)
+  (append lst (list obj)))
+
+(defun map-int (fn n)
+  (let ((acc nil))
+    (dotimes (i n)
+      (push (funcall fn i) acc))
+    (reverse acc)))
+
+(defun filter (fn lst)
+  (let ((acc nil))
+    (dolist (x lst)
+      (let ((val (funcall fn x)))
+        (if val
+            (push val acc))))
+    (reverse acc)))
+
+(defun most (fn lst)
+  (if (null lst)
+      (values nil nil)
+      (let* ((wins (car lst))
+             (max (funcall fn wins)))
+        (dolist (x (cdr lst))
+          (let ((score (funcall fn x)))
+            (if (> score max)
+                (progn (setf wins x)
+                       (setf max score)))))
+        (values wins max))))
