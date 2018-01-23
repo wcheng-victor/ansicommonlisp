ecd617279effd1a1d9f0717213d35c38673a413d
wangchengm <wangchengm@yonyou.com>
2018-01-23 16:16:24 +0800

    chapter 6 finished

--- chapter6.lisp
+++ chapter6.lisp
@@ -53,3 +53,36 @@
   (lambda (&rest args)
     (not (apply fn args))))
 
+(defun compose (&rest fns)
+  (destructuring-bind (x . y) (reverse fns)
+    (lambda (&rest args)
+      (reduce (lambda (v f)
+                (funcall f v))
+              y
+              :initial-value (apply x args)))))
+
+(defun disjoin (fn &rest fns)
+  (if (null fns)
+      fn
+      (let ((disj (apply #'disjoin fns)))
+        (lambda (&rest args)
+          (or (apply fn args) (apply disj args))))))
+
+(defun conjoin (fn &rest fns)
+  (if (null fns)
+      fn
+      (let ((conj (apply #'conjoin fns)))
+        (lambda (&rest args)
+          (and (apply fn args) (apply conj args))))))
+
+(defun curry (fn &rest args)
+  (lambda (&rest args2)
+    (apply fn (append args args2))))
+
+
+(defun rcurry (fn &rest args)
+  (lambda (&rest args2)
+    (apply fn (append args2 args))))
+
+(defun always (x)
+  (lambda (&rest args) x))
