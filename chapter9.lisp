5ab0a047830deb17763009ae36f9c04745a2942e
wangchengm <wangchengm@yonyou.com>
2018-01-26 15:14:34 +0800

    chapter 9 is added

---
+++ chapter9.lisp
@@ -0,0 +1,16 @@
+(defun sq (x) (* x x))
+
+(defun mag (x y z)
+  (sqrt (+ (sq x) (sq y) (sq z))))
+
+(defun unit-vector (x y z)
+  (let ((d (mag x y z)))
+    (values (/ x d) (/ y d) (/ z d))))
+
+(defstruct (point (:conc-name nil))
+  x y z)
+
+(defun distance (p1 p2)
+  (mag (- (x p1) (x p2))
+       (- (y p1) (y p2))
+       (- (z p1) (z p2))))
